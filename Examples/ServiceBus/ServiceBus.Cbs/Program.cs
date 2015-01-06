4b2b8275dc2ee98cf3a5658bb2c4b7e5895f2a9f
xinchen <tomchen75@hotmail.com>
2015-01-05 18:05:04 -0800

    Add license header to sample files

--- Examples/ServiceBus/ServiceBus.Cbs/Program.cs
+++ Examples/ServiceBus/ServiceBus.Cbs/Program.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Net;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using Amqp;
-using Amqp.Framing;
+﻿//  ------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation
+//  All rights reserved. 
+//  
+//  Licensed under the Apache License, Version 2.0 (the ""License""); you may not use this 
+//  file except in compliance with the License. You may obtain a copy of the License at 
+//  http://www.apache.org/licenses/LICENSE-2.0  
+//  
+//  THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+//  EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR 
+//  CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR 
+//  NON-INFRINGEMENT. 
+// 
+//  See the Apache Version 2.0 License for specific language governing permissions and 
+//  limitations under the License.
+//  ------------------------------------------------------------------------------------
 
 namespace ServiceBus.Cbs
 {
+    using System;
+    using System.Net;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Amqp;
+    using Amqp.Framing;
+
     class Program
     {
         // update the following with valid Service Bus namespace and SAS key info
