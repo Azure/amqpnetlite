3eb587c5b03e6f3a13f362fc2579af806b31a11c
xinchen <xinchen@microsoft.com>
2017-11-20 15:35:06 -0800

    Update version to 2.1.0

--- src/Properties/Version.cs
+++ src/Properties/Version.cs
@@ -24,6 +24,6 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
