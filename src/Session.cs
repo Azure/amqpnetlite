f70502cd1ed702c9af1a993c76ddbcc079973a3f
Nicolas Deslandes <ndeslandes@live.fr>
2018-07-19 23:37:46 +0100

    Fix code to add support for .NET 3.5 and .NET MF

--- src/Session.cs
+++ src/Session.cs
@@ -18,8 +18,6 @@
 namespace Amqp
 {
     using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
     using Amqp.Framing;
     using Amqp.Types;
 
@@ -619,7 +617,7 @@ namespace Amqp
             SequenceNumber first = dispose.First;
             SequenceNumber last = dispose.Last;
 
-            var disposedDeliveries = new List<Delivery>();
+            var disposedDeliveries = new List();
             lock (this.ThisLock)
             {
                 LinkedList linkedList = dispose.Role ? this.outgoingList : this.incomingList;
@@ -658,16 +656,17 @@ namespace Amqp
             // notifications to new tasks, except the last one
             for (int i = 0; i < disposedDeliveries.Count; i++)
             {
-                var delivery = disposedDeliveries[i];
+                var delivery = (Delivery)disposedDeliveries[i];
                 disposedDeliveries[i] = null;   // Avoid trailing reference
-
+#if !NETFX35 && !NETMF
                 if (i < disposedDeliveries.Count - 1)
                 {
                     // Marshall the OnStateChange call to another task so that this one doesn't get held because
                     // of potential continuations
-                    Task.Run(() => delivery.OnStateChange(dispose.State));
+                    System.Threading.Tasks.Task.Factory.StartNew(() => delivery.OnStateChange(dispose.State));
                 }
                 else
+#endif
                 {
                     // No need to marshall the last notification; we do want to avoid any context switching for the last
                     // delivery (especially in the typical case where there's only one)
