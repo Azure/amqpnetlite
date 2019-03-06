d677f79855b04e58fc75bc3105a1fdb6021f53cf
xinchen <xinchen@microsoft.com>
2019-03-06 11:13:20 -0800

    Update version to 2.1.7

--- src/Properties/Version.cs
+++ src/Properties/Version.cs
@@ -25,5 +25,5 @@ using System.Reflection;
 //      Revision
 //
 [assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.6")]
-[assembly: AssemblyInformationalVersion("2.1.6")]
+[assembly: AssemblyFileVersion("2.1.7")]
+[assembly: AssemblyInformationalVersion("2.1.7")]
