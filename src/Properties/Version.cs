4c7d3d21d5868101cb4622e726ed110242c7048f
xinchen <xinchen@microsoft.com>
2020-03-12 09:24:34 -0700

    Increase version to 2.4.0

--- src/Properties/Version.cs
+++ src/Properties/Version.cs
@@ -25,5 +25,5 @@ using System.Reflection;
 //      Revision
 //
 [assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.3.0")]
-[assembly: AssemblyInformationalVersion("2.3.0")]
+[assembly: AssemblyFileVersion("2.4.0")]
+[assembly: AssemblyInformationalVersion("2.4.0")]
