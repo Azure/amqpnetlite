08247e1b1f4c281152eba9a9d690c2b64db76edc
xinchen <xinchen@microsoft.com>
2022-02-10 19:55:02 -0800

    Increase version to 2.4.3

--- src/Properties/Version.cs
+++ src/Properties/Version.cs
@@ -25,5 +25,5 @@ using System.Reflection;
 //      Revision
 //
 [assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.4.3")]
-[assembly: AssemblyInformationalVersion("2.4.3")]
+[assembly: AssemblyFileVersion("2.4.4")]
+[assembly: AssemblyInformationalVersion("2.4.4")]
