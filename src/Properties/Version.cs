c8519e84a8b9711c1c0f680bdaac13379df8b4e7
xinchen <xinchen@microsoft.com>
2024-12-05 09:55:18 -0800

    Update version to 2.5.0

--- src/Properties/Version.cs
+++ src/Properties/Version.cs
@@ -24,6 +24,6 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.11")]
-[assembly: AssemblyInformationalVersion("2.4.11")]
+[assembly: AssemblyVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.5.0")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
