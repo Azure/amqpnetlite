58c1559eacdd63f0727380b41351792d81bf7b4f
Xin Chen <xinchen@microsoft.com>
2026-04-15 15:53:17 -0700

    Update version to 2.5.2
    Prepare for new release

--- src/Properties/Version.cs
+++ src/Properties/Version.cs
@@ -25,5 +25,5 @@ using System.Reflection;
 //      Revision
 //
 [assembly: AssemblyVersion("2.5.0")]
-[assembly: AssemblyFileVersion("2.5.1")]
-[assembly: AssemblyInformationalVersion("2.5.1")]
+[assembly: AssemblyFileVersion("2.5.2")]
+[assembly: AssemblyInformationalVersion("2.5.2")]
